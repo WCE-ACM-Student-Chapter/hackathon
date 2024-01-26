76a0db35f84e4b032d73f6507cb75aef42d81b6f
Ajay <sumitpadadune1689@gmail.com>
2024-01-26 16:40:02 +0530

    Backend Commit

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express();
+
+require('dotenv').config()
+const PORT = process.env.PORT || 4000
+app.use(express.json());
+
+// const cookieParser = require('cookie-parser');
+// app.use(cookieParser());
+ 
+
+const user = require('./routes/user')
+app.use("/api/v1",user);
+
+app.listen(PORT,()=>{
+    console.log("Server Started at PORT 3000");
+})
+
+
+const dbConnect = require('./config/database')
+dbConnect()
+
+app.get("/",(req,res)=>{
+    res.send(`App started at ${PORT}`)
+})
+
