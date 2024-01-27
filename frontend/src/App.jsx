36921522b8fb45d260078c0e287d5def189cb87a
Sharaneshwar Bharat Punjal <punjalsharaneshwar@gmail.com>
2024-01-28 01:51:53 +0530

    timeline added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import './App.css';
 import Header from './components/header/Header.jsx';
 import Home from './components/home/Home.jsx';
-import Countdown from './components/countdown/countdown.jsx';
 import About from './components/about/about.jsx';
 import Footer from './components/Footer/footer.jsx';
+import Timeline from './components/timeline/Timeline.jsx';
 
 const App = () => {
-  return (
-    <>
-      <Header />
-      <Home />
-      <About />
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<Header />
+			<Home />
+			<About />
+			<Timeline />
+			<Footer />
+		</>
+	);
 };
 
 export default App;
