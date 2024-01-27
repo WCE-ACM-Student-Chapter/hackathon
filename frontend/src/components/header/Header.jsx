bd201c7f1cf058db88296ffbb2e3dc0d9ac52221
om7057 <kulkarniom7057@gmail.com>
2024-01-27 11:40:32 +0530

    Added About Component"

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,72 +1,98 @@
-import React from 'react'
-import './header.css'
+import React, { useState } from "react";
+import "./header.css";
 
 const Header = () => {
-    return (
-        <header className="header">
-            <nav className="nav container">
-                <a href="index.html" className='nav__logo'>WCE Hackathon</a>
+  const [Toggle, showMenu] = useState(false);
 
-                <div>
-                    <ul className="nav__list grid">
-                        <li className="nav__item">
-                            <a href="#home" className="nav__link active-link">
-                                <i className="uil uil-estate nav__icon"></i> Home
-                            </a>
-                        </li>
+  const handleAboutClick = () => {
+    const aboutSection = document.getElementById("about");
 
-                        <li className="nav__item">
-                            <a href="#about" className="nav__link">
-                                <i className="uil uil-user nav__icon"></i> About
-                            </a>
-                        </li>
+    if (aboutSection) {
+      aboutSection.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    }
 
-                        <li className="nav__item">
-                            <a href="#schedule" className="nav__link">
-                                <i className="uil uil-file-alt nav__icon"></i> Schedule
-                            </a>
-                        </li>
+    // Optional: Close the menu if needed
+    showMenu(false);
+  };
 
-                        <li className="nav__item">
-                            <a href="#themes" className="nav__link">
-                                <i className="uil uil-briefcase-alt nav__icon"></i> Themes
-                            </a>
-                        </li>
+  return (
+    <header className="header">
+      <nav className="nav container">
+        <a href="index.html" className="nav__logo">
+          WCE Hackathon
+        </a>
 
-                        <li className="nav__item">
-                            <a href="#sponsors" className="nav__link">
-                                <i className="uil uil-scenery nav__icon"></i> Sponsors
-                            </a>
-                        </li>
+        <div>
+          <ul className="nav__list grid">
+            <li className="nav__item">
+              <a href="#home" className="nav__link active-link">
+                <i className="uil uil-estate nav__icon"></i> Home
+              </a>
+            </li>
 
-                        <li className="nav__item">
-                            <a href="#gallery" className="nav__link">
-                                <i className="uil uil-message nav__icon"></i> Gallery
-                            </a>
-                        </li>
+            <li className="nav__item">
+              <a
+                href="#about"
+                className="nav__link"
+                onClick={() => handleAboutClick()}
+              >
+                <i className="uil uil-user nav__icon"></i> About
+              </a>
+            </li>
 
-                        <li className="nav__item">
-                            <a href="#contact" className="nav__link">
-                                <i className="uil uil-message nav__icon"></i> Contact
-                            </a>
-                        </li>
+            <li className="nav__item">
+              <a href="#schedule" className="nav__link">
+                <i className="uil uil-file-alt nav__icon"></i> Schedule
+              </a>
+            </li>
 
-                        <li className="nav__item">
-                            <a href="#register" className="nav__link">
-                                <i className="uil uil-message nav__icon"></i> Register Now
-                            </a>
-                        </li>
-                    </ul>
+            <li className="nav__item">
+              <a href="#themes" className="nav__link">
+                <i className="uil uil-briefcase-alt nav__icon"></i> Themes
+              </a>
+            </li>
 
-                    <i className="uil uil-times nav__close" id="nav-close" onClick={() => showMenu(!Toggle)}></i>
-                </div>
+            <li className="nav__item">
+              <a href="#sponsors" className="nav__link">
+                <i className="uil uil-scenery nav__icon"></i> Sponsors
+              </a>
+            </li>
 
-                <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>
-                    <i className="uil uil-apps"></i>
-                </div>
-            </nav>
-        </header>
-    )
-}
+            <li className="nav__item">
+              <a href="#gallery" className="nav__link">
+                <i className="uil uil-message nav__icon"></i> Gallery
+              </a>
+            </li>
 
-export default Header
+            <li className="nav__item">
+              <a href="#contact" className="nav__link">
+                <i className="uil uil-message nav__icon"></i> Contact
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#register" className="nav__link">
+                <i className="uil uil-message nav__icon"></i> Register Now
+              </a>
+            </li>
+          </ul>
+
+          <i
+            className="uil uil-times nav__close"
+            id="nav-close"
+            onClick={() => showMenu(!Toggle)}
+          ></i>
+        </div>
+
+        <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>
+          <i className="uil uil-apps"></i>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
